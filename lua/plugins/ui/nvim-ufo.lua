8f4add4cc4e73e013d9341d8bff5f4264aeaa0cf
Justin Braun <justinbraun335@gmail.com>
2025-08-28 23:16:54 -0400

    fix: remove duplicate LSP setup from nvim-ufo config

--- lua/plugins/ui/nvim-ufo.lua
+++ lua/plugins/ui/nvim-ufo.lua
@@ -5,32 +5,17 @@ return {
     },
     event = "BufReadPost",
     config = function()
-        -- folding settings
-        vim.o.foldcolumn = "1" -- '0' is not bad
-        vim.o.foldlevel = 99 -- ufo needs a large value
+        -- Folding settings
+        vim.o.foldcolumn = "1"
+        vim.o.foldlevel = 99
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
-        -- keymaps
+        -- Keymaps
         vim.keymap.set("n", "zR", require("ufo").openAllFolds)
         vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-        -- setup LSP capabilities for folding
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true,
-        }
-
-        local lspconfig = require("lspconfig")
-        local servers = { "lua_ls", "basedpyright" }
-        for _, server in ipairs(servers) do
-            lspconfig[server].setup({
-                capabilities = capabilities,
-            })
-        end
-
-        -- finally setup ufo
+        -- Setup
         require("ufo").setup()
     end,
 }
