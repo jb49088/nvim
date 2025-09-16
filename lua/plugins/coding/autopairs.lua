1b1f9b96684956a60ebf4d86dca6f032f9a602e3
Justin Braun <justinbraun335@gmail.com>
2025-09-16 03:54:32 -0400

    feat: add custom rule in nvim-autopairs for python f-strings

--- lua/plugins/coding/autopairs.lua
+++ lua/plugins/coding/autopairs.lua
@@ -4,7 +4,24 @@ return {
     event = "VeryLazy",
     config = function()
         local autopairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+        local cond = require("nvim-autopairs.conds")
+
         autopairs.setup({})
+
+        -- Add custom rules for Python f-strings
+        autopairs.add_rules({
+            -- Single quotes in Python, specifically inside brackets
+            Rule("'", "'", "python")
+                :with_pair(cond.before_regex("[%[%{].*")) -- if there's a [ or { before
+                :with_pair(cond.after_regex(".*[%]%}]")), -- and a ] or } after
+
+            -- Double quotes in Python, specifically inside brackets
+            Rule('"', '"', "python")
+                :with_pair(cond.before_regex("[%[%{].*")) -- if there's a [ or { before
+                :with_pair(cond.after_regex(".*[%]%}]")), -- and a ] or } after
+        })
+
         -- Create toggle for nvim-autopairs
         Snacks.toggle({
             name = "Auto Pairing",
--- lua/plugins/utils/snacks.lua
+++ lua/plugins/utils/snacks.lua
@@ -6,27 +6,25 @@ return {
     lazy = false,
     opts = {
         --- BIGFILE ---
-        bigfile = { enabled = true },
+        bigfile = {
+            enabled = true,
+            setup = function(ctx)
+                -- Keep all the default optimizations except statuscolumn manipulation
+                if vim.fn.exists(":NoMatchParen") ~= 0 then
+                    vim.cmd([[NoMatchParen]])
+                end
+                Snacks.util.wo(0, { foldmethod = "manual", conceallevel = 0 })
+                vim.b.minianimate_disable = true
+                vim.schedule(function()
+                    if vim.api.nvim_buf_is_valid(ctx.buf) then
+                        vim.bo[ctx.buf].syntax = ctx.ft
+                    end
+                end)
+            end,
+        },
 
         --- BUFDELETE ---
         bufdelete = { enabled = true },
-
-        --- INDENT ---
-        indent = {
-            enabled = false,
-            scope = {
-                enabled = false,
-            },
-            chunk = {
-                enabled = true,
-                only_current = true,
-                char = {
-                    corner_top = "╭",
-                    corner_bottom = "╰",
-                    arrow = "─",
-                },
-            },
-        },
 
         --- LAZYGIT ---
         lazygit = { enabled = true },
@@ -187,6 +185,7 @@ return {
         require("snacks").setup(opts)
 
         vim.g.snacks_animate = false -- Disable snacks animations
+        -- vim.opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]] -- Custom status column
 
         -- toggles
         -- stylua: ignore start
