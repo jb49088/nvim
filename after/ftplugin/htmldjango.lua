bc2312df4dbba575711fe4189f63635d94139dae
Justin Braun <justinbraun335@gmail.com>
2025-10-02 23:40:11 -0400

    feat: add undo_ftplugin to properly cleanup ftplugin settings

--- after/ftplugin/htmldjango.lua
+++ after/ftplugin/htmldjango.lua
@@ -4,3 +4,7 @@ vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
 vim.bo.autoindent = true
+
+-- Cleanup on filetype change
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or "")
+    .. "| setlocal tabstop< softtabstop< shiftwidth< expandtab< autoindent<"
