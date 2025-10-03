38075601ef654223cc9b7db408636935cfbb07c8
Justin Braun <justinbraun335@gmail.com>
2025-10-03 15:06:25 -0400

    chore: cleanup leftover whichkey keymaps

--- lua/plugins/utils/whichkey.lua
+++ lua/plugins/utils/whichkey.lua
@@ -8,8 +8,6 @@ return {
         show_help = true,
         spec = {
             -- individual keymaps
-            { "<leader>ta", icon = { icon = "󰱑", hl = "MiniIconsGreen" } },
-            { "<leader>tf", icon = { icon = "󰱑", hl = "MiniIconsGreen" } },
             { "<leader>K", icon = { icon = "󰋽", hl = "MiniIconsBlue" } },
             { "<leader>cm", icon = { icon = "󱁤", hl = "MiniIconsGrey" } },
             -- groups
