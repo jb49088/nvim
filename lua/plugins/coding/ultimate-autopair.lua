1d99be678ea350367a7ed8a7b6854eae86f0bb45
Justin Braun <justinbraun335@gmail.com>
2025-11-06 23:52:28 -0500

    feat: add autopairing in search modes

--- lua/plugins/coding/ultimate-autopair.lua
+++ lua/plugins/coding/ultimate-autopair.lua
@@ -6,10 +6,19 @@ return {
     "altermo/ultimate-autopair.nvim",
     event = { "InsertEnter", "CmdlineEnter" },
     branch = "v0.6",
-    opts = {},
     config = function()
         local ua = require("ultimate-autopair")
-        ua.setup({})
+
+        ua.setup({
+            extensions = {
+                cmdtype = {
+                    skip = { "@", "-" }, -- removed '/' and '?' to enable search autopair
+                },
+            },
+            -- Add cmdline-specific quote pairs with high priority
+            { '"', '"', imap = false, cmap = true, p = 200 },
+            { "'", "'", imap = false, cmap = true, p = 200 },
+        })
 
         -- Create toggle for ultimate-autopair
         Snacks.toggle({
